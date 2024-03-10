6a7e1c01e44fba414e72efd2351ec56bd36fed2a
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-03-10 11:12:40 +0100

    All Models Created and Team should be fully made + Constants

---
+++ FootballManager-SoftuniProject.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManager_SoftuniProject.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        // Team
+        public const int TeamNameMaxLenght = 100;
+        public const int TeamNameMinLenght = 3;
+
+        public const int TeamCountryMaxLenght = 50;
+        public const int TeamCountryMinLenght = 5;
+    }
+}
---
+++ FootballManager-SoftuniProject.Infrastructure/Data/Models/Team.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FootballManager_SoftuniProject.Infrastructure.Constants.DataConstants;
+
+namespace FootballManager_SoftuniProject.Infrastructure.Data.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+            Players = new List<Player>();
+            Matches = new List<Match>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TeamNameMaxLenght)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(TeamCountryMaxLenght)]
+        public string Country { get; set; } = null!;
+
+
+        [Required]
+        public int LeagueId { get; set; }
+        [ForeignKey(nameof(LeagueId))]
+        public League League { get; set; } = null!;
+
+        [Required]
+        public int ManagerId { get; set; }
+        [ForeignKey(nameof(ManagerId))]
+        public Manager Manager { get; set; } = null!;
+
+        [Required]
+        public int StadiumId { get; set; }
+        [ForeignKey(nameof(StadiumId))]
+        public Stadium Stadium { get; set; } = null!;
+
+        public ICollection<Player> Players { get; set; }
+
+        public ICollection<Match> Matches  { get; set; }
+
+    }
+}

a0179cc3b710387f37f870384f1a267c509c048c
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-03-10 10:52:13 +0100

    Moved The DbContext and Identity in Program to ServiceExtension

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using FootballManager_SoftuniProject.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceExtensions 
+    {
+        public static IServiceCollection AddAppService(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddAppDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAppIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services
+                .AddDefaultIdentity<IdentityUser>(options => 
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireUppercase = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}

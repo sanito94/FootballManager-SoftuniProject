22f3931e87c1732720ad33f02f40cd181a9682f2
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-03-10 10:06:15 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballManager_SoftuniProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

333ea14bbc0ee1774a671a5ec053dda34174ff37
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-03-18 13:52:43 +0100

    Migration for the LeagueTable

---
+++ FootballManager-SoftuniProject.Infrastructure/Migrations/20240318125151_LeagueTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballManager_SoftuniProject.Infrastructure.Migrations
+{
+    public partial class LeagueTest1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "League",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "League");
+        }
+    }
+}
